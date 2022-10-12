9fae7533638b05e354bfdee79a56e6f4a9fe31b9
MarcosG95 <maregi@live.com>
2022-10-11 21:02:52 -0300

    Sprint 2

--- ahorcado.py
+++ ahorcado.py
@@ -1,12 +1,30 @@
 class Ahorcado:
 
-    vidas = 5
-
     def __init__(self, palabra):
         self.palabra = palabra
+        self.vidas = 5
+    
+    def getVidas(self):
+        return self.vidas
 
     def arriesgarPalabra(self, palabraSugerida):
         if self.palabra == palabraSugerida:
             return True
         else:
             return False
+
+    def arriesgaLetra(self, letraSugerida):
+        palabraJugador = []
+        respuesta = False
+        for i in range(len(self.palabra)-1):
+            palabraJugador.append(0)
+
+        for i in range(len(self.palabra)-1):
+            if self.palabra[i] == letraSugerida:
+                palabraJugador[i] = letraSugerida
+                respuesta = True
+        if respuesta == False:
+            self.vidas -= 1
+
+        return respuesta
+
--- tests.py
+++ tests.py
@@ -1,18 +1,36 @@
 import unittest
 from ahorcado import Ahorcado
-import unittest
+global juego
+juego = Ahorcado('hola')
 
 class TestArriesgarPalabra(unittest.TestCase):
     
     def test_PalabraCorrecta(self):
-        juego = Ahorcado('hola')
         resultado = juego.arriesgarPalabra('hola')
         self.assertTrue(resultado)
 
     def test_PalabraEquivocada(self):
-        juego = Ahorcado('hola')
         resultado = juego.arriesgarPalabra('chau')
         self.assertFalse(resultado)
+    
+class TestArriesgarLetra(unittest.TestCase):
+    
+    def test_LetraCorrecta(self):
+        resultado = juego.arriesgaLetra("h")
+        self.assertTrue(resultado)
+
+    def test_LetraIncorrecta(self):
+        resultado = juego.arriesgaLetra("x")
+        self.assertFalse(resultado)
+
+    def test_VidaPerdida(self):
+        resultado = juego.arriesgaLetra("h")
+        vidas1 = juego.getVidas()
+        resultado = juego.arriesgaLetra("x")
+        vidas2 = juego.getVidas()
+        self.assertNotEqual(vidas1, vidas2)
+
+
 
 if __name__ == '__main__':
     unittest.main()
