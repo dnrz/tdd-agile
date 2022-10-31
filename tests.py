1d71b190665f57bef188bd490f8e9735debd79f8
dnrz <dnruiz99@gmail.com>
2022-10-31 20:15:31 -0300

    syntax

--- tests.py
+++ tests.py
@@ -4,34 +4,19 @@ global JUEGO
 JUEGO = Ahorcado('hola')
 class TestArriesgarPalabra(unittest.TestCase): 
     def test_palabra_correcta(self):
-        """ 
-        Test Palabra correcta
-        """
         resultado = JUEGO.arriesgar_palabra('hola')
         self.assertTrue(resultado)
     def test_palabra_equivocada(self):
-        """ 
-        Test palabra equivocada
-        """
         resultado = JUEGO.arriesgar_palabra('chau')
         self.assertFalse(resultado)    
 class TestArriesgarLetra(unittest.TestCase):    
     def test_letra_correcta(self):
-        """ 
-        Test letra correcta
-        """
         resultado = JUEGO.arriesga_letra("h")
         self.assertTrue(resultado)
     def test_letra_incorrecta(self):
-        """ 
-        Test letra incorrecta
-        """
         resultado = JUEGO.arriesga_letra("x")
         self.assertFalse(resultado)
     def test_vida_perdida(self):
-        """ 
-        Test letra correcta
-        """
         JUEGO.arriesga_letra("x")
         self.assertLess(JUEGO.get_vidas(), 5)
 
