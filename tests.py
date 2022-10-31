837ca75d9a373a54931f45c4d9e5e63a5aed4d63
dnrz <dnruiz99@gmail.com>
2022-10-31 20:06:23 -0300

    syntax changes

--- tests.py
+++ tests.py
@@ -8,36 +8,36 @@ class TestArriesgarPalabra(unittest.TestCase):
         """ 
         Test Palabra correcta
         """
-        resultado = JUEGO.arriesgarPalabra('hola')
+        resultado = JUEGO.arriesgar_palabra('hola')
         self.assertTrue(resultado)
 
     def test_palabra_equivocada(self):
         """ 
         Test palabra equivocada
         """
-        resultado = JUEGO.arriesgarPalabra('chau')
+        resultado = JUEGO.arriesgar_palabra('chau')
         self.assertFalse(resultado)    
 class TestArriesgarLetra(unittest.TestCase):    
     def test_letra_correcta(self):
         """ 
         Test letra correcta
         """
-        resultado = JUEGO.arriesgaLetra("h")
+        resultado = JUEGO.arriesga_letra("h")
         self.assertTrue(resultado)
 
     def test_letra_incorrecta(self):
         """ 
         Test letra incorrecta
         """
-        resultado = JUEGO.arriesgaLetra("x")
+        resultado = JUEGO.arriesga_letra("x")
         self.assertFalse(resultado)
 
     def test_vida_perdida(self):
         """ 
         Test letra correcta
         """
-        JUEGO.arriesgaLetra("x")
-        self.assertLess(JUEGO.getVidas(), 5)
+        JUEGO.arriesga_letra("x")
+        self.assertLess(JUEGO.get_vidas(), 5)
 
 
 
