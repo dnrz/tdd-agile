f337ceb66c43de00909ad507e4f21158b9983332
daruiz1337 <david.ruiz@mercadolibre.com>
2023-03-03 20:09:13 -0300

    game

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    JUEGO = Ahorcado('palabra')
+    juego = Ahorcado('palabra')
     #for _ in range(len(JUEGO.palabra)):
     #    if JUEGO.vidas > 0:
     #        JUEGO.arriesga_letra('x')
     #        if JUEGO.vidas == 0:
     #            return 'PERDISTE'
-    return render_template('game.html',to_display=JUEGO.palabra,tries=JUEGO.vidas)
+    return render_template('game.html',to_display=juego.palabra,tries=juego.vidas)
 
 if __name__ == "__main__":
     app.run()
