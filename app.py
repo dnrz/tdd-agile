76449b5d1c62cc9d73cf33b52d6b7cdaf26f7381
daruiz1337 <david.ruiz@mercadolibre.com>
2023-03-03 19:28:41 -0300

    changes app

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from ahorcado import Ahorcado
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "boilerplate"
+    JUEGO = Ahorcado('palabra')
+    for _ in range(len(JUEGO.palabra)):
+        if JUEGO.vidas > 0:
+            JUEGO.arriesga_letra('x')
+            if JUEGO.vidas == 0:
+                return 'PERDISTE'
+
+
 
 if __name__ == "__main__":
     app.run()
