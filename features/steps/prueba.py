78f00bd5c254d97dabb9b596b2c6f74a4f873148
MarcosG95 <maregi@live.com>
2023-03-03 20:02:16 -0300

    Tests

--- features/steps/prueba.py
+++ features/steps/prueba.py
@@ -9,7 +9,7 @@ RESULTADO = True
 @given('Una partida con una palabra a adivinar')
 def step_partida(context):
     global JUEGO
-    JUEGO = Ahorcado(random.choice(PALABRAS)) 
+    JUEGO = Ahorcado(random.choice(PALABRAS))
 @when('Ingreso una letra que forma parte de la palabra')
 def step_letra_correcta(context):
     global RESULTADO
@@ -30,15 +30,19 @@ def step_palabra_incorrecta(context):
 def step_resultado_correcto(context):
     if RESULTADO:
         return 'Bien! Letra correcta'
+    return None
 @then('Se muestra mensaje de error porque la letra es equivocada')
 def step_resultado_incorrecto(context):
     if not RESULTADO:
         return 'Error! Letra equivocada'
+    return None
 @then('Se muestra un mensaje diciendo que gane el juego')
 def step_victoria(context):
     if RESULTADO:
         return 'Respuesta correcta! Felicitaciones!'
+    return None
 @then('Se muestra mensaje diciendo que perdi el juego')
 def step_game_over(context):
     if not RESULTADO:
         return 'Palabra incorrecta. Fin del juego!'
+    return None
