afbcf3914cedc459f4ee51ae516ba4e48e0e8d26
daruiz1337 <david.ruiz@mercadolibre.com>
2023-03-03 19:46:46 -0300

    render template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from ahorcado import Ahorcado
 
 app = Flask(__name__)
@@ -6,13 +6,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     JUEGO = Ahorcado('palabra')
-    for _ in range(len(JUEGO.palabra)):
-        if JUEGO.vidas > 0:
-            JUEGO.arriesga_letra('x')
-            if JUEGO.vidas == 0:
-                return 'PERDISTE'
-
-
+    #for _ in range(len(JUEGO.palabra)):
+    #    if JUEGO.vidas > 0:
+    #        JUEGO.arriesga_letra('x')
+    #        if JUEGO.vidas == 0:
+    #            return 'PERDISTE'
+	#        blanks = 0
+	#        to_display = []
+	#	    if char==" ":
+	#		    to_display.append(" ")
+	#	     else:
+	#		    to_display.append("_")
+	#		    blanks+=1
+    return render_template('game.html',to_display=JUEGO.palabra,tries=JUEGO.vidas)
 
 if __name__ == "__main__":
     app.run()
